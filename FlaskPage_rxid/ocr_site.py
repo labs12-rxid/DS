672ab4511f361b4ec33ae7d16de376e3309a212b
Carlos Gutierrez <chgutier@gmail.com>
2019-07-02 20:53:30 -0500

    Just needs some tweaks flask page to display OCR results

--- FlaskPage_rxid/ocr_site.py
+++ FlaskPage_rxid/ocr_site.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 from werkzeug.utils import secure_filename
+import requests
 import os
+import uuid
+import boto3
 
 
 app = Flask(__name__)
@@ -11,9 +14,11 @@ posts = [
     }
 ]
 
-app.config["IMAGE_UPLOADS"] = os.path.dirname(os.path.abspath(__file__))
+# app.config["IMAGE_UPLOADS"] = os.path.dirname(os.path.abspath(__file__))
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG"]
 
+# Function to check if image file is among the ones we allow
 def allowed_image(filename):
     if not "." in filename:
         return False
@@ -23,26 +28,78 @@ def allowed_image(filename):
     else:
         return False
 
+# Function to add uploaded/saved image files to S3 bucket
+s3_resource = boto3.resource('s3')
+# bucket name
+img_bucket_name = 'firstpythonbucketac60bb97-95e1-43e5-98e6-0ca294ec9aad'
+
+def add_to_s3(image_file, filepath):
+    # Looping through files to upload into S3 bucket
+    s3_resource.Object(img_bucket_name, 
+                        image_file).upload_file(
+                        Filename=filepath)
+
+
+
+
+
 @app.route("/")
-@app.route("/home", methods=["GET", "POST"])
-def home():
+def index():
+    return render_template("upload.html")
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    # Objects needed to upload to S3 and get results from database
+    url = 'http://rxid-ds.us-east-2.elasticbeanstalk.com/rekog'
+    s3_path = "https://s3.us-east-2.amazonaws.com/firstpythonbucketac60bb97-95e1-43e5-98e6-0ca294ec9aad/"
+    data = {"image_locations": ["", ""]}
+    response = requests.post(url, data)
+
     if request.method == "POST":
         if request.files:
-            image = request.files["image"]
-            if image.filename == "": # if file does not have a name
-                print("Image file must have a filename")
-                return redirect(request.url)
-            if not allowed_image(image.filename):
-                print("Your image has a file extension that is not allowed")
-                return redirect(request.url)
-            else:
-                new_filename = secure_filename(image.filename)
-                print(new_filename)
-                image.save(os.path.join(app.config["IMAGE_UPLOADS"], new_filename))
-            print("--> Image saved <--")
-            return redirect(request.url)
+            target = os.path.join(APP_ROOT, 'images/')
+            print(target)
+            # making "image" directory if does not exist
+            if not os.path.isdir(target):
+                os.mkdir(target)
+                print("Made new 'image' directory")
 
-    return render_template("home.html", posts=posts, title="Home")
+            image_list = request.files.getlist("image")
+            # making sure we only get upto 2 images
+            if len(image_list) > 2: 
+                # cutting number images to just 2
+                image_list = image_list[:2] 
+
+            i=0 # index to replace "" in data dict for S3 url
+            for image in image_list:
+                print(f'Image list: {len(image_list)}')
+                print(f"image: {image}")
+                filename = image.filename
+                # if file does not have a name
+                if filename == "":
+                    print("Image file must have a filename")
+                    return redirect(request.url)
+                # if it's not an image file with expected extension
+                if not allowed_image(filename):
+                    print("Your image has a file extension that is not allowed")
+                    return redirect(request.url)
+                else:
+                    # file extension
+                    ext = filename.rsplit(".", 1)[1] 
+                    # new unique file name
+                    new_filename = str(uuid.uuid4()) + "." + str(ext) 
+                    destination = "/".join([target, new_filename])
+                    image.save(destination)
+                    print(f"Destination: {destination}")
+                    # function to add "destination" file to S3 bucket
+                    print("---->>> Should have added to S3")
+                    add_to_s3(new_filename, destination) 
+                    data["image_locations"][i] = s3_path + new_filename
+                    print(data)
+    data = str(data)
+    print(data)
+    return render_template("results.html", response=response)
 
 
 @app.route("/about")
